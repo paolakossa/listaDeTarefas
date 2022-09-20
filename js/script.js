4e49bf8d517d70b9e775c266b17aa4342faac3b6
Paola Kossa <paolakossa@gmail.com>
2022-09-20 13:49:23 -0300

    Construction of functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const inputElement = document.querySelector('.new-task-input');
 const addTaskButton = document.querySelector('.new-task-button');
+const tasksContainer = document.querySelector('.tasks-container');
 
 const validateInput = () => inputElement.value.trim().length > 0;
 
@@ -10,7 +11,36 @@ const handleAddTask = () => {
 		return inputElement.classList.add('error'); 
 	}
 
+	const taskItemContainer = document.createElement('div');
+	taskItemContainer.classList.add('task-item');
 
+	const taskContent = document.createElement('p');
+	taskContent.innerText = inputElement.value; 
+
+	taskContent.addEventListener('click', () => handleClick()); 
+
+	const deleteItem = document.createElement('i');
+	deleteItem.classList.add('fa-regular');
+	deleteItem.classList.add('fa-trash-can');
+
+	deleteItem.addEventListener('click', () => handleDeleteClick()); 
+
+	taskItemContainer.appendChild(taskContent);
+	taskItemContainer.appendChild(deleteItem);
+
+	tasksContainer.appendChild(taskItemContainer);
+
+	inputElement.value = '';
 };
 
+const handleInputChange = () => {
+	const inputIsValid = validateInput(); 
+
+	if(inputIsValid) {
+		return inputElement.classList.remove('error'); 
+	}
+}; 
+
 addTaskButton.addEventListener('click', () => handleAddTask()); 
+
+inputElement.addEventListener('change', () => handleInputChange()); 
